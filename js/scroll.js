41ec2cf191778ddd6a6ce4d2db29fdf0c0fef3a7
luiznatoo <gabriel.naato@gmail.com>
2023-03-02 18:04:03 -0300

    otimizando a área principal e criando o layout para artigo

--- js/scroll.js
+++ js/scroll.js
@@ -6,3 +6,4 @@ ScrollReveal().reveal('.area2', { delay: 100 });
 ScrollReveal().reveal('.area3', { delay: 400 });
 ScrollReveal().reveal('.area4', { delay: 300 });
 ScrollReveal().reveal('.area5', { delay: 200 });
+baguetteBox.run('.gallery');
