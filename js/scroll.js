5486a75b078cf9618f56785cd89f51d9bd8f35cd
Luiz pc belém rio <gabriel.naato@gmail.com>
2023-03-02 10:10:04 -0300

    Terceira versão do site

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true});
+
+
+ScrollReveal().reveal('.area1', { delay: 500 });
+ScrollReveal().reveal('.area2', { delay: 100 });
+ScrollReveal().reveal('.area3', { delay: 400 });
+ScrollReveal().reveal('.area4', { delay: 300 });
+ScrollReveal().reveal('.area5', { delay: 200 });
